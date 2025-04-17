45ba62d024f1e697fddff327caad16c461ce829a
MizterFrek <jllicagarcia9506@gmail.com>
2025-04-17 00:32:34 -0500

    Ejercicio Sesion 1 - II

---
+++ src/DollarToSolConverter.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/*
+ * Semana 1 - Sesion 2
+ * Ejercicio 3
+ * Implementar un programa en Java que permita convertir dólares a soles
+ * (tipo de cambio: 3.72)
+ */
+public class DollarToSolConverter {
+
+    public static float EXCHANGE_RATE = 3.72F;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Captura de datos
+        System.out.print("Ingrese el monto en dolares ($)");
+        float dollar = scanner.nextFloat();
+
+        // Calculo
+        float sol = dollar * EXCHANGE_RATE;
+
+        // Impresion de resultado
+        System.out.printf("%.2f dolares equivale a %.2f soles: ", dollar, sol);
+    }
+
+}
---
+++ src/TemperatureConverter.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/*
+ * Semana 1 - Sesion 2
+ * Ejercicio 2
+ *
+ * Escribir un programa que convierta grados Centígrados en grados Fahrenheit.
+ * Fórmula: F = (C * 9/5) + 32
+ *
+ */
+public class TemperatureConverter {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Captura de datos
+        System.out.print("Ingrese temperatura en °C: ");
+        float celsius = scanner.nextFloat();
+
+        // Calculo
+        float fahrenheit = (celsius * 9/5) + 32;
+
+        // Impresion de resultado
+        System.out.printf("%.2f°C equivale a %.2f °F: ", celsius, fahrenheit);
+    }
+}
